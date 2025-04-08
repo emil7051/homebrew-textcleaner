bc65f7e9a78dc3def877c2ad70a54c6cc2be06ee
Edward Miller <emh.miller@me.com>
2025-04-08 14:10:11 +1000

    Simplify formula by removing virtualenv requirement

--- Formula/textcleaner.rb
+++ Formula/textcleaner.rb
@@ -1,6 +1,4 @@
 class Textcleaner < Formula
-  include Language::Python::Virtualenv
-
   desc "Text cleaning tool for LLM processing"
   homepage "https://github.com/emil7051/llm-text-processor"
   url "https://github.com/emil7051/llm-text-processor/archive/refs/tags/v0.1.0.zip"
@@ -9,28 +7,21 @@ class Textcleaner < Formula
 
   depends_on "python@3.9"
 
-  resource "beautifulsoup4" do
-    url "https://files.pythonhosted.org/packages/af/0b/44c39cf3b18a9280950ad63a579ce395dda4c32193ee9da7ff0aed547094/beautifulsoup4-4.12.2.tar.gz"
-    sha256 "492bbc69dca35d12daac71c4db1bfff0c876c00ef4a2ffacce226d4638eb72da"
-  end
-
-  resource "click" do
-    url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
-    sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
-  end
-
-  resource "pyyaml" do
-    url "https://files.pythonhosted.org/packages/cd/e5/af35f7ea75cf72f2cd079c95ee16797de7cd71f29ea7c68ae5ce7be1eda0/PyYAML-6.0.1.tar.gz"
-    sha256 "bfdf460b1736c775f2ba9f6a92bca30bc2095067b8a9d77876d1fad6cc3b4a43"
-  end
-  
-  resource "pypdf" do
-    url "https://pypi.io/packages/source/p/pypdf/pypdf-3.17.4.tar.gz"
-    sha256 "8c931e8def738d5ff8cbad4e47adcddd518759e4ff4a581480769305871b46ee"
-  end
-
   def install
-    virtualenv_install_with_resources
+    # Install the package directly using pip
+    system Formula["python@3.9"].opt_bin/"pip3", "install", "--prefix=#{prefix}", 
+           "--no-deps", "."
+
+    # Install dependencies directly
+    system Formula["python@3.9"].opt_bin/"pip3", "install", "--prefix=#{prefix}",
+           "beautifulsoup4", "click", "pypdf", "pyyaml"
+    
+    # Create a simple wrapper script
+    (bin/"textcleaner").write <<~EOS
+      #!/bin/bash
+      exec "#{Formula["python@3.9"].opt_bin}/python3" -m llm_text_processor.cli "$@"
+    EOS
+    chmod 0755, bin/"textcleaner"
   end
 
   test do
